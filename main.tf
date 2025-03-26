ccbe105eab54e152a00537088fa03b5a669922a1
meitavEini <meitav.eini@nitzanim.tech>
2025-03-26 18:15:16 +0200

    Update main.tf - change port to 80 and update SG rules

--- main.tf
+++ main.tf
@@ -2,52 +2,66 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# 1 create VPC
+# create VPC
 resource "aws_vpc" "main_vpc" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
     Name = "main_vpc_nk"
+    owner = "meitaveini"
   }
 }
 
-# connect to-Public Subnet 1 (CIDR 10.0.102.0/24)
-data "aws_subnet" "public_subnet_1" {
-  id = "subnet-0ce80abd307eac787"
-}
+# create Private Subnet 1 new
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = false
 
-# connect to-Public Subnet 2 (CIDR 10.0.101.0/24)
-data "aws_subnet" "public_subnet_2" {
-  id = "subnet-00124604db2fbc34c"
-}
-
-# connect to-Private Subnet 1 (CIDR 10.0.2.0/24) - exist
-data "aws_subnet" "private_subnet_1" {
-  id = "subnet-057de053ba24e6eb8"
+  tags = {
+    Name = "PrivateSubnet1"
+    owner = "meitaveini"
+  }
 }
 
 # create Private Subnet 2 new
 resource "aws_subnet" "private_subnet_2" {
   vpc_id                  = aws_vpc.main_vpc.id
-  cidr_block              = "10.0.103.0/24"
-  availability_zone       = "us-east-1a"
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1e"
   map_public_ip_on_launch = false
 
   tags = {
     Name = "PrivateSubnet2"
+    owner = "meitaveini"
   }
 }
 
-# connect to-Private Subnet 2 (CIDR 10.0.102.0/24)
-data "aws_subnet" "private_subnet_2" {
-  id = "subnet-0ce80abd307eac787"
+# create Public Subnet 1 new
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.3.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "PublicSubnet1"
+    owner = "meitaveini"
+  }
 }
 
+# create Public Subnet 2 new
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.4.0/24"
+  availability_zone       = "us-east-1e"
+  map_public_ip_on_launch = true
 
-# Private Subnet
-resource "aws_subnet" "private_subnet" {
-  vpc_id     = aws_vpc.main_vpc.id
-  cidr_block = "10.0.2.0/24"
+  tags = {
+    Name = "PublicSubnet2"
+    owner = "meitaveini"
+  }
 }
 
 # create Internet Gateway
@@ -56,6 +70,7 @@ resource "aws_internet_gateway" "main_igw" {
 
   tags = {
     Name = "MainInternetGateway"
+    owner = "meitaveini"
   }
 }
 # attached Elastic IP
@@ -70,6 +85,7 @@ resource "aws_nat_gateway" "nat_gw" {
 
   tags = {
     Name = "NAT-Gateway"
+    owner = "meitaveini"
   }
 }
 
@@ -79,6 +95,7 @@ resource "aws_route_table" "public_rt" {
 
   tags = {
     Name = "Public-RT"
+    owner = "meitaveini"
   }
 }
 
@@ -95,6 +112,7 @@ resource "aws_route_table" "private_rt" {
 
   tags = {
     Name = "Private-RT"
+    owner = "meitaveini"
   }
 }
 
@@ -105,22 +123,18 @@ resource "aws_route" "private_to_nat" {
   nat_gateway_id         = aws_nat_gateway.nat_gw.id
 }
 
-
-# connect-Route Table of Public Subnets
-# connect Public Subnets to-Route Table public
 resource "aws_route_table_association" "public_subnet_1_association" {
-  subnet_id      = data.aws_subnet.public_subnet_1.id
+  subnet_id      = aws_subnet.public_subnet_1.id
   route_table_id = aws_route_table.public_rt.id
 }
 
 resource "aws_route_table_association" "public_subnet_2_association" {
-  subnet_id      = data.aws_subnet.public_subnet_2.id
+  subnet_id      = aws_subnet.public_subnet_2.id
   route_table_id = aws_route_table.public_rt.id
 }
 
-# connect Private Subnets to-Route Table private
 resource "aws_route_table_association" "private_subnet_1_association" {
-  subnet_id      = data.aws_subnet.private_subnet_1.id
+  subnet_id      = aws_subnet.private_subnet_1.id
   route_table_id = aws_route_table.private_rt.id
 }
 
@@ -128,8 +142,6 @@ resource "aws_route_table_association" "private_subnet_2_association" {
   subnet_id      = aws_subnet.private_subnet_2.id
   route_table_id = aws_route_table.private_rt.id
 }
-
-
 
 # Security Group for-Bastion
 resource "aws_security_group" "bastion_sg" {
@@ -152,7 +164,7 @@ resource "aws_security_group" "bastion_sg" {
 
 # create Bastion Host
 resource "aws_instance" "bastion_host" {
-  ami                         = "ami-08d4ac5b634553e16" # Ubuntu 22.04
+  ami                         = "ami-084568db4383264d4" # Ubuntu 24.04
   instance_type               = "t2.micro"
   subnet_id                   = aws_subnet.public_subnet_1.id
   security_groups             = [aws_security_group.bastion_sg.id]
@@ -177,8 +189,8 @@ resource "aws_security_group" "private_sg" {
   }
 
   ingress {
-    from_port       = 8000
-    to_port         = 8000
+    from_port       = 80
+    to_port         = 80
     protocol        = "tcp"
     security_groups = [aws_security_group.lb_sg.id] # connect only from-LB
   }
@@ -191,40 +203,13 @@ resource "aws_security_group" "private_sg" {
   }
 }
 
-# create private instances (Ubuntu)
-resource "aws_instance" "private_instance_1" {
-  ami             = "ami-08d4ac5b634553e16"
-  instance_type   = "t2.medium"
-  subnet_id       = data.aws_subnet.private_subnet_1.id
-  security_groups = [aws_security_group.private_sg.id]
-  key_name        = "noakirel-keypair"
-
-  tags = {
-    Name  = "PrivateServer1"
-    owner = "meitaveini"
-  }
-}
-
-resource "aws_instance" "private_instance_2" {
-  ami             = "ami-08d4ac5b634553e16"
-  instance_type   = "t2.medium"
-  subnet_id       = aws_subnet.private_subnet_2.id 
-  security_groups = [aws_security_group.private_sg.id]
-  key_name        = "noakirel-keypair"
-
-  tags = {
-    Name  = "PrivateServer2"
-    owner = "meitaveini"
-  }
-}
-
 # Security Group for Load Balancer
 resource "aws_security_group" "lb_sg" {
   vpc_id = aws_vpc.main_vpc.id
 
   ingress {
-    from_port   = 8000
-    to_port     = 8000
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"] 
   }
@@ -237,21 +222,6 @@ resource "aws_security_group" "lb_sg" {
   }
 }
 
-# Public Subnet 1 (AZ1)
-resource "aws_subnet" "public_subnet_1" {
-  vpc_id                  = aws_vpc.main_vpc.id
-  cidr_block              = "10.0.101.0/24" 
-  map_public_ip_on_launch = true
-  availability_zone       = "us-east-1a"
-}
-
-# Public Subnet 2 (AZ2)
-resource "aws_subnet" "public_subnet_2" {
-  vpc_id                  = aws_vpc.main_vpc.id
-  cidr_block              = "10.0.102.0/24" 
-  map_public_ip_on_launch = true
-  availability_zone       = "us-east-1b"
-}
 # Load Balancer with 2 subnets
 resource "aws_lb" "web_lb" {
   name               = "web-load-balancer"
@@ -268,27 +238,15 @@ resource "aws_lb" "web_lb" {
 # create Target Group
 resource "aws_lb_target_group" "web_tg" {
   name        = "web-target-group"
-  port        = 8000
+  port        = 80
   protocol    = "HTTP"
   vpc_id      = aws_vpc.main_vpc.id
   target_type = "instance"
 }
 
-# connect the instances to-TG
-resource "aws_lb_target_group_attachment" "instance_1" {
-  target_group_arn = aws_lb_target_group.web_tg.arn
-  target_id        = aws_instance.private_instance_1.id
-}
-
-resource "aws_lb_target_group_attachment" "instance_2" {
-  target_group_arn = aws_lb_target_group.web_tg.arn
-  target_id        = aws_instance.private_instance_2.id
-}
-
-# create Listener in-LB for-TG
 resource "aws_lb_listener" "web_listener" {
   load_balancer_arn = aws_lb.web_lb.arn
-  port              = 8000
+  port              = 80
   protocol          = "HTTP"
 
   default_action {
@@ -296,22 +254,35 @@ resource "aws_lb_listener" "web_listener" {
     target_group_arn = aws_lb_target_group.web_tg.arn
   }
 }
+
+#resource "aws_lb_listener" "web_listener_https" {
+#  load_balancer_arn = aws_lb.web_lb.arn
+#  port              = 443
+#  protocol          = "HTTPS"
+#  ssl_policy        = "ELBSecurityPolicy-2016-08" # במידה ויש תעודת SSL
+#  certificate_arn   = "arn:aws:acm:...."           # במידה ויש תעודת SSL
+
+#  default_action {
+#    type             = "forward"
+#    target_group_arn = aws_lb_target_group.web_tg.arn
+#  }
+#}
+
 # AWS Auto Scaling Group with User Data and Tag Filtering
 
 resource "aws_launch_template" "statuspage_lt" {
   name_prefix   = "statuspage-lt-"
-  image_id      = "ami-08d4ac5b634553e16" # Ubuntu 22.04
+  image_id      = "ami-084568db4383264d4"
   instance_type = "t2.medium"
   key_name      = "noakirel-keypair"
+  update_default_version = true
 
-  # Load user-data script (base64 encoded)
-  user_data = filebase64("${path.module}/docs/user-data.sh")
+  user_data = filebase64("${path.module}/docs/user-data.sh") 
 
   vpc_security_group_ids = [aws_security_group.private_sg.id]
 
   tag_specifications {
     resource_type = "instance"
-
     tags = {
       Name  = "statuspage-prod"
       owner = "meitaveini"
@@ -320,12 +291,13 @@ resource "aws_launch_template" "statuspage_lt" {
   }
 }
 
+
 resource "aws_autoscaling_group" "statuspage_asg" {
   name                      = "statuspage-asg"
   desired_capacity          = 2
   min_size                  = 2
   max_size                  = 2
-  vpc_zone_identifier       = [data.aws_subnet.private_subnet_1.id, aws_subnet.private_subnet_2.id]
+  vpc_zone_identifier       = [aws_subnet.private_subnet_1.id, aws_subnet.private_subnet_2.id]
   health_check_type         = "EC2"
   health_check_grace_period = 300
   target_group_arns         = [aws_lb_target_group.web_tg.arn]
