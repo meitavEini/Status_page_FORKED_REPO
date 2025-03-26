c7497ac5be9bf7418310dec57c91b7a338fc9d76
meitavEini <meitav.eini@nitzanim.tech>
2025-03-26 16:56:08 +0200

    adding new user data file

--- docs/user-data.sh
+++ docs/user-data.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-# Update and install basic packages
-apt update && apt install -y git docker.io docker-compose
+# Update system and install basic packages
+sudo apt update && sudo apt install -y git docker.io docker-compose # apt update && apt install -y git docker.io docker-compose
 
-# Clone your repository (replace with your repo)
-mkdir -p /opt/status-page
+# Add ubuntu user to docker group
+sudo usermod -aG docker ubuntu # sudo usermod -aG sudo ubuntu
+
+newgrp docker # newgrp sudo
+
+# Clone your repository
+mkdir -p /opt/status-page # sudo mkdir -p /opt/status-page
 cd /opt/status-page
 git clone https://github.com/meitavEini/Status_page_FORKED_REPO.git .
 git checkout main
 
+sleep 10
+
 # Run the app
-docker compose -f docker-compose.yml up -d --build
+sudo docker-compose -f docker-compose.yml up -d --build
